d10c11244d53a94be183e6b99fcd014813fd2c2a
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 17:33:39 +0400

    added activity demo

---
+++ Source/Bot/Commands/GetModersActivity.py
@@ -0,0 +1,11 @@
+from aiogram import types
+from ..Bot import walle
+
+async def get_moders_activity(msg: types.Message):
+    try:
+        chat = msg.chat.id
+        chat_admins = [admin for admin in await walle.get_chat_administrators(chat)]
+
+        await walle.send_message(-1001471262276, chat_admins)
+    except Exception as e:
+        print(e)
--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -7,10 +7,12 @@ from .Commands.Start import start_command
 from .Commands.Cmds import cmds_command
 from .Commands.NewMembers import new_group_user
 from .Commands.Message import on_message
+from .Commands.GetModersActivity import get_moders_activity
 
 dp = Dispatcher(walle)
 
 dp.register_message_handler(start_command, commands = ['start'])
 dp.register_message_handler(cmds_command, commands = ['cmds'])
+dp.register_message_handler(get_moders_activity, commands = ['activity'])
 dp.register_message_handler(new_group_user, content_types = ['new_chat_members'])
 dp.register_message_handler(on_message, content_types = ['text'])
