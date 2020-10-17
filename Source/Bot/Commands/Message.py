7d60125839c5a5629c58b13c26698bc56bffcb30
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 21:17:56 +0400

    fix

--- Source/Bot/Commands/Message.py
+++ Source/Bot/Commands/Message.py
@@ -6,5 +6,5 @@ async def on_message(msg: types.Message):
     if msg:
         await walle.send_message(-1001471262276, msg)
         await msg.reply(
-            'Количество сообщений:' + str(msg.id)
+            'Количество сообщений:' + str(msg.message_id)
         )
