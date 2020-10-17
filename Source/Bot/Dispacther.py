06d68a3c82a6bcbd6a130668c9bebbbfdaadaddd
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 16:19:01 +0400

    added cmds command

--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -4,8 +4,9 @@ from .Bot import walle
 
 # Importing bot commands
 from .Commands.Start import start_command
-
+from .Commands.Cmds import cmds_command
 
 dp = Dispatcher(walle)
 
 dp.register_message_handler(start_command, commands = ['start'])
+dp.register_message_handler(cmds_command, commands = ['cmds'])
