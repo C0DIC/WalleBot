57aec1b8dc3f33987bc2d98f29e491df6a1aecec
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 17:23:21 +0400

    fixed channel

--- Source/Bot/Commands/Message.py
+++ Source/Bot/Commands/Message.py
@@ -4,4 +4,4 @@ from ..Bot import walle
 
 async def on_message(msg: types.Message):
     if msg:
-        await walle.send_message(-1001331703781, msg)
+        await walle.send_message(-1001471262276, msg)
