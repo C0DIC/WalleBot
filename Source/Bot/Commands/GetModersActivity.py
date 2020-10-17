0ed2ef3b3a3715249aaeb8fe0a4e7b48e288fa7c
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 17:44:29 +0400

    fix

--- Source/Bot/Commands/GetModersActivity.py
+++ Source/Bot/Commands/GetModersActivity.py
@@ -6,6 +6,10 @@ async def get_moders_activity(msg: types.Message):
         chat = msg.chat.id
         chat_admins = [admin.user.first_name for admin in await walle.get_chat_administrators(chat)]
 
+        admins = dict(admin: 0 for admin in chat_admins)
+
+        print(admins)
+
         await walle.send_message(-1001471262276, chat_admins)
     except Exception as e:
         print(e)
