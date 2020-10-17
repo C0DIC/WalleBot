cc5f7e7d7ff0fd457c37da654be7ac83b8db4037
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-17 16:17:36 +0400

    added cmds command

---
+++ Source/Bot/Commands/Cmds.py
@@ -0,0 +1,17 @@
+from aiogram import types
+from ..Bot import walle
+
+async def cmds_command(msg: types.Message):
+    try:
+        walle_commands = [command for command in await walle.get_my_commands()]
+        cmds_command_text = ''
+
+        for times in range(0, len(walle_commands)):
+            cmds_command_text += walle_commands[times].description + ': ' + walle_commands[times].command + '\n'
+
+        await msg.answer(
+            cmds_command_text
+        )
+
+    except Exception as e:
+        print(e)
