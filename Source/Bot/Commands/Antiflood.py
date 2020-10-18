220eff144117b43bb260fd14bdc6360949302036
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-18 20:07:35 +0400

    antiflood demo

---
+++ Source/Bot/Commands/Antiflood.py
@@ -0,0 +1,15 @@
+import asyncio
+from aiogram import types
+from ..Bot import walle
+
+
+async def antiflood(msg: types.Message):
+    try:
+        msg_start = {msg.from_user.first_name : msg.message_id}
+        await asyncio.sleep(7)
+        msg_stop = {msg.from_user.first_name : msg.message_id}
+
+        if msg_start == msg_stop:
+            print('Flood')
+    except Exception as e:
+        print(e)
--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -15,7 +15,7 @@ from .Commands.Ban import ban_command
 from .Commands.Unban import unban_command
 from .Commands.Pin import pin_command
 from .Commands.ActionsFilter import actions_filter
-
+from .Commands.Antiflood import antiflood
 
 dp = Dispatcher(walle)
 
@@ -29,6 +29,7 @@ dp.register_message_handler(new_group_user, content_types = ['new_chat_members']
 dp.register_message_handler(left_group_user, content_types = ['left_chat_member'])
 dp.register_message_handler(dp.async_task(remove_system_msg), content_types = ['pinned_message'])
 dp.register_message_handler(actions_filter, content_types = ['text'])
+dp.register_message_handler(antiflodd, content_types = ['any'])
 
 # Moderation
 dp.register_message_handler(mute_command, commands = ['mute'], commands_prefix = '*')
