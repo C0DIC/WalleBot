feb6d15f40990ecda5092566bf2cd54d789cec2d
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-18 17:22:53 +0400

    fixes

--- Source/Bot/Commands/ActionsFilter.py
+++ Source/Bot/Commands/ActionsFilter.py
@@ -10,7 +10,22 @@ async def actions_filter(msg: types.Message):
     sndr = msg.from_user
     target = msg.reply_to_message.from_user
 
+    for i in actions_f and actions_m:
+        if action in actions_f[i] or action in actions_m[i]:
+            await msg.answer(
+            '[{}]({}) {} [{}]({}) \| 😱'.format(
+                returnNoneReserved(sndr.first_name),
+                sndr.url,
+                returnNoneReserved(action),
+                returnNoneReserved(target.first_name),
+                target.url
+            ),
+            parse_mode = 'MarkdownV2'
+        )
+
+
     if action in actions_m or actions_f:
+        await msg.answer(action)
         await msg.answer(
             '[{}]({}) {} [{}]({}) \| 😱'.format(
                 returnNoneReserved(sndr.first_name),
@@ -18,5 +33,6 @@ async def actions_filter(msg: types.Message):
                 returnNoneReserved(action),
                 returnNoneReserved(target.first_name),
                 target.url
-            )
+            ),
+            parse_mode = 'MarkdownV2'
         )
