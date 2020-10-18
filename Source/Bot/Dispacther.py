97ed24e1a0b109d40d8d3e564d19f37e1a87a1a5
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-18 20:09:22 +0400

    fixing

--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -29,7 +29,7 @@ dp.register_message_handler(new_group_user, content_types = ['new_chat_members']
 dp.register_message_handler(left_group_user, content_types = ['left_chat_member'])
 dp.register_message_handler(dp.async_task(remove_system_msg), content_types = ['pinned_message'])
 dp.register_message_handler(actions_filter, content_types = ['text'])
-dp.register_message_handler(antiflodd, content_types = ['any'])
+dp.register_message_handler(antiflood, content_types = ['any'])
 
 # Moderation
 dp.register_message_handler(mute_command, commands = ['mute'], commands_prefix = '*')
