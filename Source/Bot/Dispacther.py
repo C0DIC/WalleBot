9bffb755dd394dcc0d01207a9bd8db9523b837ce
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-22 15:12:39 +0400

    fixes

--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -26,12 +26,13 @@ dp.register_message_handler(cmds_command, commands = ['cmds'])
 dp.register_message_handler(start_command, commands = ['start'])
 dp.register_message_handler(stat_command, commands = ['stat'])
 
+
 # Events
 dp.register_message_handler(new_group_user, content_types = ['new_chat_members'])
 dp.register_message_handler(left_group_user, content_types = ['left_chat_member'])
 dp.register_message_handler(dp.async_task(remove_system_msg), content_types = ['pinned_message'])
 dp.register_message_handler(dp.async_task(actions_filter), content_types = ['text'])
-#dp.register_message_handler(antiflood, content_types = ['any'])
+
 
 # Moderation
 dp.register_message_handler(mute_command, commands = ['mute'])
