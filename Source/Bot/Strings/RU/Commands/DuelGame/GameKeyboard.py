9b16f3a04573a5937a674d1a367286ddb11c973e
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-22 17:24:15 +0400

    fixes

--- Source/Bot/Strings/RU/Commands/DuelGame/GameKeyboard.py
+++ Source/Bot/Strings/RU/Commands/DuelGame/GameKeyboard.py
@@ -3,6 +3,7 @@ from aiogram import types
 yes_button = types.InlineKeyboardButton('Да!', callback_data = 'yes')
 no_button = types.InlineKeyboardButton('Нет!', callback_data = 'no')
 
-buttons_list = [yes_button, no_button]
 
-game_keyboard = types.InlineKeyboardMarkup(2, buttons_list)
+game_keyboard = types.InlineKeyboardMarkup(2)
+game_keyboard.add(yes_button)
+game_keyboard.add(no_button)
