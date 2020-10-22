3e3d7b74a4c7985432e9703abdb98dc1aa518e17
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-22 18:59:10 +0400

    fixing

--- Source/Bot/Commands/DuelGame/Duel.py
+++ Source/Bot/Commands/DuelGame/Duel.py
@@ -27,65 +27,69 @@ async def duel_command(msg: types.Message):
                 parse_mode = 'MarkdownV2',
                 reply_markup = GameKeyboard.game_keyboard
             )
-
-            async def duel_callb_listener(call: types.CallbackQuery):
-                modes = ['win', 'draw']
-                members = [target.from_user, sndr]
-
-                if call.from_user == sndr:
-                    await msg.answer(
-                        DuelShizoid.shizoid_duel.format(
-                            sndr.url
-                        ),
-                        parse_mode = 'MarkdownV2'
-                    )
-                elif call.from_user != target.from_user:
-                    await msg.answer(
-                        DuelNotForYou.not_for_you.format(
-                            returnNoneReserved(call.from_user.first_name),
-                            call.from_user.url
-                        ),
-                        parse_mode = 'MarkdownV2'
-                    )
-                else:
-                    if call.data == 'yes':
-                        mode = random.choice(modes)
-
-                        if mode == 'win':
-                            winner = random.choice(members)
-
-                            await msg.answer(
-                                Win.win_text.format(
-                                    returnNoneReserved(winner.first_name),
-                                    winner.url,
-                                    returnNoneReserved(call.from_user.first_name),
-                                    call.from_user.url,
-                                    returnNoneReserved(winner.first_name),
-                                    winner.url
-                                ),
-                                parse_mode = 'MarkdownV2'
-                            )
-                        elif mode == 'draw':
-                            winner = random.choice(members)
-
-                            await msg.answer(
-                                DrawGame.draw_game_text.format(
-                                    returnNoneReserved(winner.first_name),
-                                    winner.url,
-                                    returnNoneReserved(call.from_user.first_name),
-                                    call.from_user.url,
-                                    returnNoneReserved(call.from_user.first_name),
-                                    call.from_user.url
-                                ),
-                                parse_mode = 'MarkdownV2'
-                            )
-
-                    elif call.data == 'no':
-                        await msg.reply(
-                            DuelDenied.denied_duel,
-                            parse_mode = 'MarkdownV2'
-                        )
     except DeprecationWarning:
         pass
     except Exception as e:
         print(e)
+
+
+async def duel_callb_listener(call: types.CallbackQuery):
+    sndr = call.message.reply_to_message.from_user
+    target = call.message.reply_to_message.reply_to_message
+
+    modes = ['win', 'draw']
+    members = [target.from_user, sndr]
+
+    if call.from_user == sndr:
+        await call.message.answer(
+            DuelShizoid.shizoid_duel.format(
+                sndr.url
+                ),
+                parse_mode = 'MarkdownV2'
+            )
+    elif call.from_user != target.from_user:
+        await call.message.answer(
+            DuelNotForYou.not_for_you.format(
+                returnNoneReserved(call.from_user.first_name),
+                call.from_user.url
+            ),
+            parse_mode = 'MarkdownV2'
+        )
+    else:
+        if call.data == 'yes':
+            mode = random.choice(modes)
+
+            if mode == 'win':
+                winner = random.choice(members)
+
+                await call.message.answer(
+                    Win.win_text.format(
+                        returnNoneReserved(winner.first_name),
+                        winner.url,
+                        returnNoneReserved(call.from_user.first_name),
+                        call.from_user.url,
+                        returnNoneReserved(winner.first_name),
+                        winner.url
+                    ),
+                    parse_mode = 'MarkdownV2'
+                )
+            elif mode == 'draw':
+                winner = random.choice(members)
+
+                await call.message.answer(
+                    DrawGame.draw_game_text.format(
+                        returnNoneReserved(winner.first_name),
+                        winner.url,
+                        returnNoneReserved(call.from_user.first_name),
+                        call.from_user.url,
+                        returnNoneReserved(call.from_user.first_name),
+                        call.from_user.url
+                    ),
+                    parse_mode = 'MarkdownV2'
+                )
+
+        elif call.data == 'no':
+            await call.message.reply(
+                DuelDenied.denied_duel,
+                parse_mode = 'MarkdownV2'
+            )
--- Source/Bot/Dispacther.py
+++ Source/Bot/Dispacther.py
@@ -17,7 +17,7 @@ from .Commands.Pin import pin_command
 from .Commands.ActionsFilter import actions_filter
 from .Commands.Antiflood import antiflood
 from .Commands.CallAdmins import call_admins_command
-from .Commands.DuelGame.Duel import duel_command
+from .Commands.DuelGame.Duel import duel_command, duel_callb_listener
 
 
 dp = Dispatcher(walle)
@@ -43,4 +43,4 @@ dp.register_message_handler(new_group_user, content_types = ['new_chat_members']
 dp.register_message_handler(left_group_user, content_types = ['left_chat_member'])
 dp.register_message_handler(dp.async_task(remove_system_msg), content_types = ['pinned_message'])
 dp.register_message_handler(dp.async_task(actions_filter), content_types = ['text'])
-dp.register_callback_query_handler(duel_command, lambda callb_query: True)
+dp.register_callback_query_handler(duel_callb_listener, lambda callb_query: True)
