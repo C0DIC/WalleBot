e242cad616059b7b73a28c801ff7cdf4f7046d28
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-10-22 17:26:47 +0400

    fixes

--- Source/Bot/Strings/RU/Commands/DuelGame/GameKeyboard.py
+++ Source/Bot/Strings/RU/Commands/DuelGame/GameKeyboard.py
@@ -4,6 +4,6 @@ yes_button = types.InlineKeyboardButton('Да!', callback_data = 'yes')
 no_button = types.InlineKeyboardButton('Нет!', callback_data = 'no')
 
 
-game_keyboard = types.InlineKeyboardMarkup(2)
+game_keyboard = types.InlineKeyboardMarkup(1)
 game_keyboard.add(yes_button)
 game_keyboard.add(no_button)
