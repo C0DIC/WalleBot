5b7b7a29e43ccb7d67e9fec7b41f09e42a50dfd4
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-11-12 02:07:57 +0400

    actions 2.0 demo

--- Source/Utils/ReadBeforeAction.py
+++ Source/Utils/ReadBeforeAction.py
@@ -1,10 +1,11 @@
 def readBeforeAction(text):
     new_text = ''
-    for i in text:
-        if i == '[':
-            new_text += i
-        if i != ']':
-            new_text += i
-        else:
-            break
+    if '[' in text or ']' in text:
+        for i in text:
+            if i == '[':
+                new_text += i
+            if i != ']':
+                new_text += i
+            else:
+                break
     return new_text
