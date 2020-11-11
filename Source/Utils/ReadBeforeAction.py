93d4798164af56983710543e1f7f148b287d3238
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-11-12 02:01:09 +0400

    actions 2.0 fixes

--- Source/Utils/ReadBeforeAction.py
+++ Source/Utils/ReadBeforeAction.py
@@ -1,10 +1,10 @@
 def readBeforeAction(text):
     new_text = ''
-     for i in text:
-         if i == '[':
-             new_text += i
-         if i != ']':
-             new_text += i
-         else:
-             break
-     return new_text
+    for i in text:
+        if i == '[':
+            new_text += i
+        if i != ']':
+            new_text += i
+        else:
+            break
+    return new_text
