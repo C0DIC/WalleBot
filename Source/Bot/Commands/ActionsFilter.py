a0ffdf29f72c812409b0be49a8c29817654dfc6d
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-11-12 18:35:04 +0400

    actions 4.1 demo2

--- Source/Bot/Commands/ActionsFilter.py
+++ Source/Bot/Commands/ActionsFilter.py
@@ -39,7 +39,7 @@ async def actions_filter(msg: types.Message):
                 if compareUsers(sndr, target.from_user):
                     pass
                 else:
-                    action = returnNoneReserved(msg_text.replace('~', ''))
+                    action = returnNoneReserved(msg_text).replace('~', '')
 
                     if isSndrSym(action) is False and isTargetSym(action) is False:
                         action = removeUnderline(action)
