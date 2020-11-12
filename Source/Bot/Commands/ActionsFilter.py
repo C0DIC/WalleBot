d0734ed18f2f949c0867884a26927522a532e8ea
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-11-12 18:42:00 +0400

    actions 4.1 demo3

--- Source/Bot/Commands/ActionsFilter.py
+++ Source/Bot/Commands/ActionsFilter.py
@@ -65,7 +65,7 @@ async def actions_filter(msg: types.Message):
                             act_target = f'[{returnNoneReserved(target.from_user.first_name)}]({target.from_user.url})'
                             action = action.replace('_', act_target)
 
-                        action = action + ' \| 💬'
+                        action = returnNoneReserved(action + ' | 💬')
 
                         await msg.answer(
                             action,
